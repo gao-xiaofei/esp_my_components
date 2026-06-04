6e52374550756b198feb0f2737dbe5e2eea27216
gao <gao_saifei@163.com>
2026-06-04 07:58:51 -0400

    feature（epd display）:增加按钮控制3个图片的切换

---
+++ components/doso_device_epd/image_test.h
@@ -0,0 +1,10 @@
+#ifndef _IMAGE_TEST_H_
+#define _IMAGE_TEST_H_
+
+#include <stdint.h>
+
+extern const unsigned char gImage_1[48000];
+extern const unsigned char gImage_2[48000];
+extern const unsigned char gImage_3[48000];
+
+#endif
